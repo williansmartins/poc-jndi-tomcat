521827f592a1d45be44712c9b4708291b4d18d24
will <professorwillians@gmail.com>
2015-05-02 11:07:42 -0300

    resolvendo conflitos

--- src/main/java/br/com/waiso/dao/especific/ICachorroDAO.java
+++
@@ -1,8 +0,0 @@
-package br.com.waiso.dao.especific;
-
-import br.com.waiso.dao.generic.Dao;
-import br.com.waiso.entity.Cachorro;
-
-public interface ICachorroDAO extends Dao<Cachorro> {
-	
-}
--- src/main/java/br/com/waiso/dao/especific/IClienteDAO.java
+++
@@ -1,8 +0,0 @@
-package br.com.waiso.dao.especific;
-
-import br.com.waiso.dao.generic.Dao;
-import br.com.waiso.entity.ClienteEntity;
-
-public interface IClienteDAO extends Dao<ClienteEntity> {
-	
-}
