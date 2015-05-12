0b52fa5891c2823a09ec2d58ae16feaaf3e90543
will <professorwillians@gmail.com>
2015-05-12 01:21:12 -0300

    add arquivos

--- src/main/java/br/com/waiso/dao/especific/IPessoaDAO.java
+++
@@ -1,8 +0,0 @@
-package br.com.waiso.dao.especific;
-
-import br.com.waiso.dao.generic.Dao;
-import br.com.waiso.entity.PessoaEntity;
-
-public interface IPessoaDAO extends Dao<PessoaEntity> {
-	
-}
